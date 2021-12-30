a5240ac86b81df29e01a513dc8c5bd68f689529e
Roxaniii <92252445+Roxaniii@users.noreply.github.com>
2021-12-30 17:50:17 +0100

    Update JS function displayTemperature

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
   let descriptionElement = document.querySelector("#description");
-  descriptionElement = response.data.weather[0].description;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "f570a1fbc37130aef5bf06a2e40664d1";
