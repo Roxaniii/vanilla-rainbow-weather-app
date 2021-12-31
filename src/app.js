3124522f2e0183381062319665e23dab913ef8e3
Roxaniii <92252445+Roxaniii@users.noreply.github.com>
2021-12-31 17:47:10 +0100

    Create JS date & update JS weather conditions

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ function displayTemperature(response) {
   cityElement.innerHTML = response.data.name;
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "f570a1fbc37130aef5bf06a2e40664d1";
@@ -13,3 +17,31 @@ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${a
 
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
+
+//Date
+let now = new Date();
+console.log(now);
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let seconds = now.getSeconds();
+let year = now.getFullYear();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let day = days[now.getDay()];
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let month = months[now.getMonth()];
+let dateElement = document.querySelector("#date");
+dateElement.innerHTML = `${day} ${month} ${date}, ${hours}:${minutes},${seconds}, ${year}`;
